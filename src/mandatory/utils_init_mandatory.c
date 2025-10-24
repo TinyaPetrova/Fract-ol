0e1f1aed71db6904cfb14c3ee69bd0e71cf93f09
Tinya Petrova <98020199+TinyaPetrova@users.noreply.github.com>
2025-10-24 23:26:38 +0200

    refactor fractal initialization and setup functions

--- src/mandatory/utils_init_mandatory.c
+++ src/mandatory/utils_init_mandatory.c
@@ -5,60 +5,56 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: upetrova <upetrova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/07 13:30:00 by upetrova          #+#    #+#             */
-/*   Updated: 2025/08/07 16:37:36 by upetrova         ###   ########.fr       */
+/*   Created: 2025/08/07 13:31:42 by upetrova          #+#    #+#             */
+/*   Updated: 2025/08/07 16:37:10 by upetrova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-void	init_fractal_base(t_fractal *f)
+int	init_mlx(t_fractal *fractal)
 {
-	f->zoom = 1.0;
-	f->offset_x = 0.0;
-	f->offset_y = 0.0;
-	f->max_iter = MAX_ITER;
-	f->julia_re = -0.7;
-	f->julia_im = 0.27015;
-	f->needs_redraw = 0;
-	f->palette = 1;
-	f->mouse_lock = 0;
-}
-
-void	init_julia_fractal(t_fractal *f, char **argv, int argc)
-{
-	f->type = JULIA;
-	if (argc == 4)
-	{
-		f->julia_re = ft_atof(argv[2]);
-		f->julia_im = ft_atof(argv[3]);
-	}
-}
-
-int	close_exit_m(t_fractal *fractal)
-{
-	mlx_destroy_image(fractal->mlx, fractal->img.img);
-	mlx_destroy_window(fractal->mlx, fractal->win);
-	mlx_destroy_display(fractal->mlx);
-	free(fractal->mlx);
-	exit(0);
-	return (0);
+	fractal->mlx = mlx_init();
+	if (!fractal->mlx)
+		return (0);
+	fractal->win = mlx_new_window(fractal->mlx, WIDTH, HEIGHT, "fract-ol");
+	if (!fractal->win)
+		return (0);
+	fractal->img.img = mlx_new_image(fractal->mlx, WIDTH, HEIGHT);
+	if (!fractal->img.img)
+		return (0);
+	fractal->img.addr = mlx_get_data_addr(fractal->img.img, &fractal->img.bpp,
+			&fractal->img.length, &fractal->img.endian);
+	return (1);
 }
 
 void	setup_hooks_m(t_fractal *fractal)
 {
-	mlx_hook(fractal->win, CLOSE_EVENT, NO_MASK, close_exit_m, fractal);
+	mlx_hook(fractal->win, 17, 0, close_exit_m, fractal);
 	mlx_key_hook(fractal->win, handle_key_m, fractal);
 	mlx_mouse_hook(fractal->win, handle_mouse_m, fractal);
 }
 
 void	init_fractal_m(t_fractal *f, char **argv, int argc)
 {
-	init_fractal_base(f);
+	f->zoom = 1.0;
+	f->offset_x = 0.0;
+	f->offset_y = 0.0;
+	f->max_iter = 100;
 	if (!ft_strncmp(argv[1], "mandelbrot", 10))
 		f->type = MANDELBROT;
 	else if (!ft_strncmp(argv[1], "julia", 5))
-		init_julia_fractal(f, argv, argc);
-	else
-		exit(1);
+	{
+		f->type = JULIA;
+		if (argc == 4)
+		{
+			f->julia_re = ft_atof(argv[2]);
+			f->julia_im = ft_atof(argv[3]);
+		}
+		else
+		{
+			f->julia_re = -0.8;
+			f->julia_im = 0.156;
+		}
+	}
 }
